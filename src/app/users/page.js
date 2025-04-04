bfcc12b6e45b8db1366ac0084461117629be6802
marco garbini <marcoogarbini@gmail.com>
2025-04-04 18:46:20 +0200

    Create component ferment-card

---
+++ src/app/users/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      hola soy la ruta de los Usuarios
+    </div>
+  );
+}
+
+export default Page;
